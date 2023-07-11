2c5081179f58a62b5d7442b701d4353cfa4d93ef
Semppi <100435776+Semppi@users.noreply.github.com>
2023-07-11 20:09:21 +0300

    Satyr Skull Model and Texture Fixes

--- src/main/java/net/semppi/semppis_mythical_legends_mod/block/entity/SatyrSkullBlockEntity.java
+++ src/main/java/net/semppi/semppis_mythical_legends_mod/block/entity/SatyrSkullBlockEntity.java
@@ -7,8 +7,11 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import net.semppi.semppis_mythical_legends_mod.block.ModBlockEntities;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class SatyrSkullBlockEntity extends BlockEntity {
+public class SatyrSkullBlockEntity extends BlockEntity implements IAnimatable {
     public SatyrSkullBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.SATYR_SKULL.get(), pos, state);
     }
@@ -16,7 +19,17 @@ public class SatyrSkullBlockEntity extends BlockEntity {
     @Override
     public @NotNull ModelData getModelData() {
         /// TODO: We need to implement here the code to load the textures and json file with the mod
-        return super.getModelData();
+        return new SatyrSkullBlockModel();
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {
+
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return null;
     }
 
     // Additional methods and logic specific to the SatyrSkull block entity
